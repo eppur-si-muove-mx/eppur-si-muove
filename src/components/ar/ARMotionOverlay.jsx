a83ca60df529cbfcf24fd8c4698d59ca93cc6eba
isaacMagma <isaac@magmapartners.com>
2025-10-02 22:28:11 -0600

    ya se ve bonis

--- src/components/ar/ARMotionOverlay.jsx
+++ src/components/ar/ARMotionOverlay.jsx
@@ -3,20 +3,14 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Rotate3D, ScanEye } from "lucide-react";
+import GyroAxisIndicator from "@/components/ar/GyroAxisIndicator";
 
 export default function ARMotionOverlay({ enableMotion, debug, onEnableCamera }) {
     return (
-        <div className="fixed top-0 left-0 right-0 bottom-0 z-10 flex flex-col gap-2 p-4 align-end justify-start">
-
-            {/* Gyroscope */}
-            <div className="text-white font-mono text-sm rounded-md pointer-events-none flex gap-4 ml-auto">
-                <div>α {debug.alpha.toFixed(1)}°</div>
-                <div>β {debug.beta.toFixed(1)}°</div>
-                <div>γ {debug.gamma.toFixed(1)}°</div>
-            </div>
-
+        <div className="fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-between gap-2 p-4">
+            
             {/* Grant Access to sensors */}
-            <div className="flex items-center align-center gap-2 w-fit ml-auto text-white font-semibold">
+            <div className="text-white font-semibold flex gap-4 h-fit items-center">
                 Sensors
                 <div className="flex gap-1 items-center text-white font-semibold justify-end">
                     <Button
@@ -37,9 +31,16 @@ export default function ARMotionOverlay({ enableMotion, debug, onEnableCamera })
                         <ScanEye />
                     </Button>
                 </div>
-
-
             </div>
+
+            {/* Gyroscope */}
+            <div className="text-white font-semibold flex gap-4 flex-col items-end w-fit">
+                <GyroAxisIndicator alpha={debug.alpha} beta={debug.beta} gamma={debug.gamma} size={24} />
+                <div>α {debug.alpha.toFixed(1)}°</div>
+                <div>β {debug.beta.toFixed(1)}°</div>
+                <div>γ {debug.gamma.toFixed(1)}°</div>
+            </div>
+
         </div>
     );
 }
